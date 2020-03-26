b85ceecce509e5be978515a4706a12223e891fa1
Dawud Ibrahim Ismail <ismaildawud96@gmail.com>
2020-03-26 14:02:57 +0000

    modules

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/idawud/gobyexample/morestring/strReverse"
+)
 
 func main() {
-	fmt.Println("one")
+	fmt.Println(strReverse.ReverseRunes("one"))
 }
